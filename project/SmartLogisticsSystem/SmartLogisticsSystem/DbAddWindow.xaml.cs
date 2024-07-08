ee7a650a616ea29ac3677c11ef6daeba6ebb5395
JinyongSon <wlsdydths@naver.com>
2024-07-08 15:52:48 +0900

    ui

---
+++ project/SmartLogisticsSystem/SmartLogisticsSystem/DbAddWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SmartLogisticsSystem
+{
+    /// <summary>
+    /// DbAddWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class DbAddWindow : Window
+    {
+        public DbAddWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+    }
+}
---
+++ project/SmartLogisticsSystem/SmartLogisticsSystem/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartLogisticsSystem
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            DbAddWindow dbAddWindow = new DbAddWindow();
+            dbAddWindow.Show();
+        }
+
+        private void BtnView_Click(object sender, RoutedEventArgs e)
+        {
+            Inventory inventory = new Inventory();
+            inventory.Show();
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}

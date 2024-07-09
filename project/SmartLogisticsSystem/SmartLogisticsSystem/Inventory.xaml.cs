60b39a3970d82c3286abbace114952247771919d
JinyongSon <wlsdydths@naver.com>
2024-07-09 17:29:51 +0900

    Db 작업

--- project/SmartLogisticsSystem/SmartLogisticsSystem/Inventory.xaml.cs
+++ project/SmartLogisticsSystem/SmartLogisticsSystem/Inventory.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using SmartLogisticsSystem.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,15 +29,117 @@ namespace SmartLogisticsSystem
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
 
-        private void GrdResults_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
         // 실시간조회 버튼 클릭
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            List<SmartLogistics> smartLogistics = new List<SmartLogistics>();
 
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helpers.Common.CONNSTRING))
+                {
+                    conn.Open();
+
+                    var cmd = new SqlCommand(Models.SmartLogistics.SELECT_QUERY, conn);
+                    var adapter = new SqlDataAdapter(cmd);
+                    var dSet = new DataSet();
+                    adapter.Fill(dSet, "SmartLogistics");
+
+                    foreach (DataRow row in dSet.Tables["SmartLogistics"].Rows)
+                    {
+                        var smartlogistcs = new SmartLogistics()
+                        {
+                            Id = Convert.ToInt32(row["Id"]),
+                            Division = Convert.ToString(row["Division"]),
+                            Product = Convert.ToString(row["Product"]),
+                            Date = Convert.ToDateTime(row["Date"]),
+                        };
+
+                        smartLogistics.Add(smartlogistcs);
+                    }
+                    DgvResult.ItemsSource = smartLogistics;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("데이터 조회 중 오류가 발생했습니다: " + ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (DgvResult.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("삭제할 품목을 선택하세요");
+            }
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helpers.Common.CONNSTRING))
+                {
+                    conn.Open();
+
+                    var delRes = 0;
+
+                    foreach (SmartLogistics item in DgvResult.SelectedItems)
+                    {
+                        SqlCommand cmd = new SqlCommand(Models.SmartLogistics.DELETE_QUERY, conn);
+                        cmd.Parameters.AddWithValue("@Id", item.Id);
+
+                        delRes += cmd.ExecuteNonQuery();
+                    }
+
+                    if (delRes == DgvResult.SelectedItems.Count)
+                    {
+                        MessageBox.Show($"{delRes}건 삭제");
+                    }
+                    else
+                    {
+                        MessageBox.Show($"{DgvResult.SelectedItems.Count}건중 {delRes}건 삭제");
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"삭제 오류 {ex.Message}");
+            }
+
+            Button_Click(sender, e);
+        }
+
+        private void DgvResult_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if (DgvResult.SelectedItem is SmartLogistics selectedSmartLogistics)
+            {
+                IdTextBox.Text = selectedSmartLogistics.Id.ToString();
+                ProdTextBox.Text = selectedSmartLogistics.Product.ToString();
+                DatePicker.SelectedDateTime = selectedSmartLogistics.Date.Date;
+                if (selectedSmartLogistics.Division.ToString() == "레드")
+                {
+                    RedCheck.IsChecked = true;
+                    BlueCheck.IsChecked = false;
+                    GreenCheck.IsChecked = false;
+                }
+                else if (selectedSmartLogistics.Division.ToString() == "블루")
+                {
+                    RedCheck.IsChecked = false;
+                    BlueCheck.IsChecked = true;
+                    GreenCheck.IsChecked = false;
+                }
+                else if (selectedSmartLogistics.Division.ToString() == "그린")
+                {
+                    RedCheck.IsChecked = false;
+                    BlueCheck.IsChecked = false;
+                    GreenCheck.IsChecked = true;
+                }
+                else // 다른 제품이라면 모든 체크를 해제
+                {
+                    RedCheck.IsChecked = false;
+                    BlueCheck.IsChecked = false;
+                    GreenCheck.IsChecked = false;
+                }
+            }
         }
     }
 }
---
+++ project/SmartLogisticsSystem/SmartLogisticsSystem/Models/SmartLogistics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLogisticsSystem.Models
+{
+    public class SmartLogistics
+    {
+        public int Id { get; set; }
+        public string Division { get; set; }
+        public string Product { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public static readonly string SELECT_QUERY = @"SELECT [Id]
+                                                              ,[Division]
+                                                              ,[Product]
+                                                              ,[Date]
+                                                          FROM [dbo].[SmartLogistics]";
+
+        public static readonly string DELETE_QUERY = @"DELETE FROM [dbo].[SmartLogistics] WHERE Id = @id";
+
+
+    }
+}
